1a8107461d1723e401f9bf3abe7c76dbe316ba43
AminaM211 <148769375+AminaM211@users.noreply.github.com>
2025-10-02 14:31:31 +0200

    feat: restructure message handling with new API routes and improve organization

--- index.js
+++ index.js
@@ -3,40 +3,11 @@ const logger = require('./middelware/logger');
 const e = require('express');
 const app = express();
 const port = 3000;
+const messagesRouter = require('./routers/v1/messages');
 
-let messages = [ 
-    "Hello Samplats",
-    "Hello Again",
-    "Hello Once More"
-];
-
-app.use("/messages", logger);
+app.use("/api/v1/messages", messagesRouter);
 app.use(express.json());
 
-app.get("/messages",  (req, res)=>{
-    res.json(messages); 
-});
-
-app.get("/messages/:id",  (req, res)=>{ 
-    let id = req.params.id;
-    if(id >= messages.length){
-        res.json({
-            "status": "error",
-            "message": "This message does not exist"
-        });
-    }
-
-    else{
-        res.json(messages[id]);
-    }
-});
- 
-
-app.post("/messages", (req, res)=>{
-    let message = req.body.message;
-    messages.push(message);
-    res.send("POST /messages");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ routers/v1/messages.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+let messages = [ 
+    "Hello Amina",
+    "Hello Again",
+    "How are you?" ,
+];
+
+
+router.get("/",  (req, res)=>{
+    res.json({
+        "status": "success",
+        "data": messages
+    }); 
+});
+
+router.get("/:id",  (req, res)=>{ 
+    let id = req.params.id;
+    if(id >= messages.length){
+        res.json({
+            "status": "error",
+            "message": "This message does not exist"
+        });
+    }
+
+    else{
+        res.json(messages[id]);
+    }
+});
+ 
+
+router.post("/", (req, res)=>{
+    let message = req.body.message;
+    messages.push(message);
+    res.send("POST /messages");
+});
+
+module.exports = router;
